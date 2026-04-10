3d0af04d820ff39dd453d08fb89a137ee80f12bf
ceccav <ceciliacavallin@hotmail.se>
2026-04-10 15:10:35 +0200

    This feature does a little bit more than intended
    Firstly i created a playgame page and made sure start game goes to /game
    Then i created some tests for the newgamepage making sure all that i could see worked
    with the help of playwright. I the separated the steps files so that common steps have its own
    file.

--- tests/playwright/e2e/ui/steps/newgamepage.steps.js
+++ tests/playwright/e2e/ui/steps/newgamepage.steps.js
@@ -1,11 +1,9 @@
 import { createBdd } from 'playwright-bdd';
 import { expect } from '@playwright/test';
 
-const { Given, When, Then } = createBdd();
+const { Given } = createBdd();
 
-//
-// NAVIGATION
-//
+
 Given('I am on the newgame page', async ({ page }) => {
   await page.goto('/newgame');
 });
